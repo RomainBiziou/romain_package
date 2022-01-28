c59e0a59f600e6f03933ccd14786d8c9c9bd0d25
RomainBiziou <romain.biziou@esqese.ucly.fr>
2022-01-28 14:02:20 +0100

    script modification

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/sh
 
-#cd ~/romain_project/src/romain_package/.git
-#WORKTREE=~/romain_project/
-
-#CHANGED=$(git --work-tree=${WORKTREE} status --porcelain)
-
-#if [ -n "${CHANGED}" ]; then
-#  echo 'changed';
-
-#else
-#  echo 'not changed';
-#fi
-
 [ -z "`git status --porcelain`" ] && echo "NULL-NO DIFFS" || echo "DIFFS EXIST"
