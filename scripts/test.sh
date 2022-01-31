f5431dcc5ba0796cf12b207206af6e819cb82e1d
RomainBiziou <romain.biziou@esqese.ucly.fr>
2022-01-31 12:03:36 +0100

    test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/sh
 
-if ! git --git-dir="~/.git" diff --quiet
-then
-    echo "change"
-fi
-
-#[ -z "`git status --porcelain`" ] && echo "Pas de differences" || echo "Differences"
+git remote show origin
